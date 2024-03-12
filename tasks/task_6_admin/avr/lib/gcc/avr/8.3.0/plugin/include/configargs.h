cef0f95a85796722f07bd0e9cea205df8029297c
yusufsakr <yusuf.sakr.eng@gmail.com>
2024-03-12 14:24:14 +0200

    Task 6 & 7

---
+++ tasks/task_6_admin/avr/lib/gcc/avr/8.3.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/yusuf/crosstool-ng/.build/avr/src/gcc/configure --build=x86_64-build_pc-linux-gnu --host=x86_64-build_pc-linux-gnu --target=avr --prefix=/home/yusuf/x-tools/avr --with-local-prefix=/home/yusuf/x-tools/avr/avr --with-headers=/home/yusuf/x-tools/avr/avr/include --with-newlib --enable-threads=no --disable-shared --with-pkgversion='crosstool-NG 1.24.0-dirty' --disable-__cxa_atexit --disable-libgomp --disable-libmudflap --disable-libmpx --disable-libssp --disable-libquadmath --disable-libquadmath-support --with-gmp=/home/yusuf/crosstool-ng/.build/avr/buildtools --with-mpfr=/home/yusuf/crosstool-ng/.build/avr/buildtools --with-mpc=/home/yusuf/crosstool-ng/.build/avr/buildtools --with-isl=/home/yusuf/crosstool-ng/.build/avr/buildtools --enable-lto --with-host-libstdcxx='-static-libgcc -Wl,-Bstatic,-lstdc++,-Bdynamic -lm' --enable-target-optspace --disable-nls --enable-multiarch --enable-languages=c,c++";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };

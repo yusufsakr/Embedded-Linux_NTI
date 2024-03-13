55221b1bf218416be4eaf16f813b4c51591cc10a
yusufsakr <yusuf.sakr.eng@gmail.com>
2024-03-13 13:20:42 +0200

    Task_7

---
+++ tasks/task_7_admin/lib/modulus.c
@@ -0,0 +1,21 @@
+/*
+ * Author : Yusuf Sakr
+ *
+ * GitHUB : github.com/yusufsakr
+ *
+ * Date   : 4, March 2024
+ */
+
+//--------------------------------------------------------------------
+
+/*
+ * Name        : modulus
+ * Arguments   : double x, double y
+ * Return      : int
+ * Description : Function recieves two numbers and returns the 
+ *               remainder of deviding the first sent number by 
+ *               the second one.
+ */
+int modulus (double x, double y) {
+	return (int)x % (int)y;
+}
---
+++ tasks/task_7_admin/lib/multiplication.c
@@ -0,0 +1,24 @@
+/*
+ * Author : Yusuf Sakr
+ * 
+ * GitHUB : yusufsakr
+ *
+ * Date   : 4, March 2024
+ */
+
+//--------------------------------------------
+
+/*
+ * Name        : multiplication
+ * Arguments   : double x, double y
+ * Return      : double
+ * Describtion : Function Recieves two numbers and return their 
+ *               Multiplicated Value.
+ */
+double multiplication (double x, double y) {
+        return x * y;
+}
+
+
+
+
